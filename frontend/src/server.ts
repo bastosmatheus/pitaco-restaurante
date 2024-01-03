9c0efb514b9c812f358972bebf1c6b0e2d87c878
Matheus <matheusbastosandrade@gmail.com>
2024-01-03 13:13:01 -0300

    :sparkles: feat: criando as rotas para o frontend e pasta public

---
+++ frontend/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { URL } from "url";
+
+const app = express();
+const __dirname = new URL(".", import.meta.url).pathname.slice(1);
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/login", (req, res) => {
+  res.sendFile(__dirname + "/pages/login.html");
+});
+
+app.use("/dashboard", (req, res) => {
+  res.sendFile(__dirname + "/pages/dashboard.html");
+});
+
+app.use("/register", (req, res) => {
+  res.sendFile(__dirname + "/pages/register.html");
+});
+
+app.use("/menu", (req, res) => {
+  res.sendFile(__dirname + "/pages/menu.html");
+});
+
+app.listen(8000);
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["frontend/src/scripts/*.ts"],
-  target: "es2020",
-  watch: true,
-  outDir: "frontend/dist",
-  format: "esm",
-});
